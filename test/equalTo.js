5d8a653aa22c95d65a113d6c2de8d7e5005710cf
Golo Roden <webmaster@goloroden.de>
2013-05-07 15:39:12 +0200

    Fixed a bug related to enumerable is property on Object.prototype.

--- test/equalTo.js
+++ test/equalTo.js
@@ -2,6 +2,10 @@
 
 var assert = require('../lib/assert.js');
 
+var Foo = function () {
+  this.bar = 'bar';
+};
+
 suite('is.equalTo', function () {
   test('equal => ok', function () {
     assert.doesNotThrow(function () {
@@ -11,6 +15,15 @@ suite('is.equalTo', function () {
   test('not equal => exception', function () {
     assert.throws(function () {
       assert.that('foo', is.equalTo('bar'));
+    });
+  });
+  suite('for objects', function () {
+    test('equal => ok', function () {
+      assert.doesNotThrow(function () {
+        assert.that(new Foo(), is.equalTo({
+          bar: 'bar'
+        }));
+      });
     });
   });
 });
