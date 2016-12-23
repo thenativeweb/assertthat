ca5384cd3cf87379420755fd1946b02ce1f9d3d5
Golo Roden <webmaster@goloroden.de>
2016-12-23 19:20:24 +0100

    Update dependencies.

--- roboter.js
+++ roboter.js
@@ -13,5 +13,35 @@ roboter.
     task('universal/test-units', {
       src: 'test/**/*Tests.js'
     });
+
+    task('universal/license', {
+      compatible: [
+        // Individual licenses
+        'Apache-2.0', 'Apache-2.0*',
+        'BSD-2-Clause', 'BSD-3-Clause',
+        'ISC',
+        'MIT*', 'MIT/X11',
+        'MIT Licensed. http://www.opensource.org/licenses/mit-license.php',
+        'Public Domain',
+        'Unlicense',
+
+        // Combined licenses
+        '(Apache-2.0 OR MPL-1.1)',
+        'BSD-3-Clause OR MIT',
+        '(MIT AND CC-BY-3.0)',
+        '(WTFPL OR MIT)'
+      ],
+
+      ignore: {
+        // BSD-3-Clause, see https://github.com/deoxxa/duplexer2/blob/0.0.2/LICENSE.md
+        duplexer2: '0.0.2',
+
+        // MIT, see https://github.com/mklabs/node-fileset/blob/v0.2.1/LICENSE-MIT
+        fileset: '0.2.1',
+
+        // MIT, see https://github.com/eugeneware/unique-stream/blob/v1.0.0/LICENSE
+        'unique-stream': '1.0.0'
+      }
+    });
   }).
   start();
